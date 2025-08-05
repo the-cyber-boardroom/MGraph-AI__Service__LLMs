fd443d347b1c9e42756a29c1ffc1b868eecb1b49
Dinis Cruz <dinis.cruz@owasp.org>
2025-08-06 00:17:34 +0100

    added missing routes (in test)

--- tests/unit/fast_api/test_Service__Fast_API__client.py
+++ tests/unit/fast_api/test_Service__Fast_API__client.py
@@ -7,6 +7,7 @@ from starlette.testclient                                             import Tes
 from osbot_fast_api_serverless.utils.testing.skip_tests               import skip__if_not__in_github_actions
 from mgraph_ai_service_llms.fast_api.Service__Fast_API                import Service__Fast_API
 from mgraph_ai_service_llms.fast_api.routes.Routes__Info              import ROUTES_PATHS__INFO, ROUTES_INFO__HEALTH__RETURN_VALUE
+from mgraph_ai_service_llms.fast_api.routes.Routes__LLMs              import ROUTES_PATHS__LLMS
 from tests.unit.Service__Fast_API__Test_Objs                          import setup__service_fast_api_test_objs, Service__Fast_API__Test_Objs, TEST_API_KEY__NAME
 
 
@@ -55,4 +56,5 @@ class test_Service__Fast_API__client(TestCase):
             assert _.is_local_stack_configured_and_available() is True
 
     def test__config_fast_api_routes(self):
-        assert self.fast_api.routes_paths() == sorted(ROUTES_PATHS__INFO)
+        assert self.fast_api.routes_paths() == sorted(ROUTES_PATHS__INFO +
+                                                      ROUTES_PATHS__LLMS)
